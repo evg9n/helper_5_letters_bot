7fad45aa623baae818ac259607db63c251ca2c50
evg9n <e.evgeny999@gmail.com>
2023-07-21 19:32:22 +0300

    edit download bot's token

--- loader.py
+++ loader.py
@@ -1,6 +1,11 @@
 from telebot import TeleBot
-from bot_token import TOKEN
 from telebot.storage import StateMemoryStorage
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = environ.get('TOKEN')
 
 storage = StateMemoryStorage()
+
 bot = TeleBot(token=TOKEN, state_storage=storage)
