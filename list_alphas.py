eb161a8c03a219d12bd152dd186438b64e11729b
Емельянов Евгений <e.evgeny999@gmail.com>
2023-08-16 22:14:18 +0300

    относительный путь -> абсолютный путь

--- list_alphas.py
+++ list_alphas.py
@@ -1,6 +1,7 @@
-import os
+from os.path import abspath
+
+
 def list_alphas():
-    print(os.path.abspath('utils/letters.txt'))
-    with open('letters.txt', 'r', encoding='utf-8') as f:
+    with open(abspath('letters.txt'), 'r', encoding='utf-8') as f:
         alphas = f.read()
     return alphas
